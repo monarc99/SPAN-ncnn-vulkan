de88f39bb908d0ab4c6251a089950151b5ba3a17
TNTwise <tntwise3@gmail.com>
2024-01-30 21:50:10 -0600

    add stuff

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Get argument for platform
-PLATFORM=$1
-
-BIN_PATH="build-$PLATFORM/upscayl-bin"
-ADDITIONAL_ARGS="-i images/input2.jpg -o test.jpg -s 4 -m models/ -n realesrgan-x4plus -c"
-# Run upscayl-bin
-$BIN_PATH $ADDITIONAL_ARGS
